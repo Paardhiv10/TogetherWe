a3b00d8500539a6241a5aa818f30abdabfea893a
Mohit Mohan Giri <73680245+Mohit0135@users.noreply.github.com>
2023-03-19 04:55:42 +0530

    Update responses.js

--- static/script/responses.js
+++ static/script/responses.js
@@ -1,18 +1,12 @@
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
-
-    // Simple responses
-    if (input == "hello") {
-        return "Hello there!";
+    if (input == "I want help") {
+        return "Please dail on 1228";
+    } else if (input == "I want to join community") {
+        return "A person will be direct to you for assistance ";
+    } else if (input == "Size self help groups") {
+        return "Size of 5 members";
     } else if (input == "goodbye") {
-        return "Talk to you later!";
+        return "Thank you for you time";
     } else {
         return "Try asking something else!";
     }
